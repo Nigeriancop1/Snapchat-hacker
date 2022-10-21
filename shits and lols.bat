5d28be112c583b26788039b74b1f4b7174d3d361
Nigeriancop1 <108397938+Nigeriancop1@users.noreply.github.com>
2022-10-22 09:32:40 +1100

    Add files via upload

---
+++ shits and lols.bat
@@ -0,0 +1,150 @@
+@echo off
+:start
+color 0C
+title NigerianCop
+echo          d8b                           d8b                                              
+echo          Y8P                           Y8P                                              
+echo                                                                                                                   
+echo 88888b.  888  .d88b.   .d88b.  888d888 888  8888b.  88888b.   .d8888b  .d88b.  88888b.  
+echo 888 "88b 888 d88P"88b d8P  Y8b 888P"   888     "88b 888 "88b d88P"    d88""88b 888 "88b
+echo 888  888 888 888  888 88888888 888     888 .d888888 888  888 888      888  888 888  888 
+echo 888  888 888 Y88b 888 Y8b.     888     888 888  888 888  888 Y88b.    Y88..88P 888 d88P 
+echo 888  888 888  "Y88888  "Y8888  888     888 "Y888888 888  888  "Y8888P  "Y88P"  88888P"  
+echo                   888                                                          888      
+echo              Y8b d88P                                                          888      
+echo               "Y88P"                                                           888    
+ECHO 1. Snapchat
+ECHO 2. Facebook
+ECHO 3. Twitter
+ECHO 4. Instagram
+ECHO 5. All
+
+CHOICE /C 12345 /M "Enter your choice:"
+
+IF ERRORLEVEL 5 GOTO All
+IF ERRORLEVEL 4 GOTO Instagram
+IF ERRORLEVEL 3 GOTO Twitter
+IF ERRORLEVEL 2 GOTO Facebook
+IF ERRORLEVEL 1 GOTO Snapchatload
+
+:Snapchatload
+cls
+echo          d8b                           d8b                                              
+echo          Y8P                           Y8P                                              
+echo                                                                                                                   
+echo 88888b.  888  .d88b.   .d88b.  888d888 888  8888b.  88888b.   .d8888b  .d88b.  88888b.  
+echo 888 "88b 888 d88P"88b d8P  Y8b 888P"   888     "88b 888 "88b d88P"    d88""88b 888 "88b
+echo 888  888 888 888  888 88888888 888     888 .d888888 888  888 888      888  888 888  888 
+echo 888  888 888 Y88b 888 Y8b.     888     888 888  888 888  888 Y88b.    Y88..88P 888 d88P 
+echo 888  888 888  "Y88888  "Y8888  888     888 "Y888888 888  888  "Y8888P  "Y88P"  88888P"  
+echo                   888                                                          888      
+echo              Y8b d88P                                                          888      
+echo               "Y88P"                                                           888    
+echo [ ] Injecting into browser.
+timeout /t 2 >nul
+goto Snapchatload2
+
+:Snapchatload2
+cls
+echo          d8b                           d8b                                              
+echo          Y8P                           Y8P                                              
+echo                                                                                                                   
+echo 88888b.  888  .d88b.   .d88b.  888d888 888  8888b.  88888b.   .d8888b  .d88b.  88888b.  
+echo 888 "88b 888 d88P"88b d8P  Y8b 888P"   888     "88b 888 "88b d88P"    d88""88b 888 "88b
+echo 888  888 888 888  888 88888888 888     888 .d888888 888  888 888      888  888 888  888 
+echo 888  888 888 Y88b 888 Y8b.     888     888 888  888 888  888 Y88b.    Y88..88P 888 d88P 
+echo 888  888 888  "Y88888  "Y8888  888     888 "Y888888 888  888  "Y8888P  "Y88P"  88888P"  
+echo                   888                                                          888      
+echo              Y8b d88P                                                          888      
+echo               "Y88P"                                                           888    
+echo [ ] Injecting into browser..
+timeout /t 2 >nul
+goto Snapchatload3
+
+:Snapchatload3
+cls
+echo          d8b                           d8b                                              
+echo          Y8P                           Y8P                                              
+echo                                                                                                                   
+echo 88888b.  888  .d88b.   .d88b.  888d888 888  8888b.  88888b.   .d8888b  .d88b.  88888b.  
+echo 888 "88b 888 d88P"88b d8P  Y8b 888P"   888     "88b 888 "88b d88P"    d88""88b 888 "88b
+echo 888  888 888 888  888 88888888 888     888 .d888888 888  888 888      888  888 888  888 
+echo 888  888 888 Y88b 888 Y8b.     888     888 888  888 888  888 Y88b.    Y88..88P 888 d88P 
+echo 888  888 888  "Y88888  "Y8888  888     888 "Y888888 888  888  "Y8888P  "Y88P"  88888P"  
+echo                   888                                                          888      
+echo              Y8b d88P                                                          888      
+echo               "Y88P"                                                           888    
+echo [ ] Injecting into browser...
+timeout /t 2 >nul
+goto Snapchatload4
+
+:Snapchatload4
+cls
+echo          d8b                           d8b                                              
+echo          Y8P                           Y8P                                              
+echo                                                                                                                   
+echo 88888b.  888  .d88b.   .d88b.  888d888 888  8888b.  88888b.   .d8888b  .d88b.  88888b.  
+echo 888 "88b 888 d88P"88b d8P  Y8b 888P"   888     "88b 888 "88b d88P"    d88""88b 888 "88b
+echo 888  888 888 888  888 88888888 888     888 .d888888 888  888 888      888  888 888  888 
+echo 888  888 888 Y88b 888 Y8b.     888     888 888  888 888  888 Y88b.    Y88..88P 888 d88P 
+echo 888  888 888  "Y88888  "Y8888  888     888 "Y888888 888  888  "Y8888P  "Y88P"  88888P"  
+echo                   888                                                          888      
+echo              Y8b d88P                                                          888      
+echo               "Y88P"                                                           888    
+echo [ ] Injecting into browser.
+timeout /t 2 >nul
+goto Snapchatload5
+
+:Snapchatload5
+cls
+echo          d8b                           d8b                                              
+echo          Y8P                           Y8P                                              
+echo                                                                                                                   
+echo 88888b.  888  .d88b.   .d88b.  888d888 888  8888b.  88888b.   .d8888b  .d88b.  88888b.  
+echo 888 "88b 888 d88P"88b d8P  Y8b 888P"   888     "88b 888 "88b d88P"    d88""88b 888 "88b
+echo 888  888 888 888  888 88888888 888     888 .d888888 888  888 888      888  888 888  888 
+echo 888  888 888 Y88b 888 Y8b.     888     888 888  888 888  888 Y88b.    Y88..88P 888 d88P 
+echo 888  888 888  "Y88888  "Y8888  888     888 "Y888888 888  888  "Y8888P  "Y88P"  88888P"  
+echo                   888                                                          888      
+echo              Y8b d88P                                                          888      
+echo               "Y88P"                                                           888    
+echo [ ] Injecting into browser..
+timeout /t 2 >nul
+goto Snapchatloadfinished
+
+:Snapchatloadfinished
+cls
+echo          d8b                           d8b                                              
+echo          Y8P                           Y8P                                              
+echo                                                                                                                   
+echo 88888b.  888  .d88b.   .d88b.  888d888 888  8888b.  88888b.   .d8888b  .d88b.  88888b.  
+echo 888 "88b 888 d88P"88b d8P  Y8b 888P"   888     "88b 888 "88b d88P"    d88""88b 888 "88b
+echo 888  888 888 888  888 88888888 888     888 .d888888 888  888 888      888  888 888  888 
+echo 888  888 888 Y88b 888 Y8b.     888     888 888  888 888  888 Y88b.    Y88..88P 888 d88P 
+echo 888  888 888  "Y88888  "Y8888  888     888 "Y888888 888  888  "Y8888P  "Y88P"  88888P"  
+echo                   888                                                          888      
+echo              Y8b d88P                                                          888      
+echo               "Y88P"                                                           888    
+echo [!] Injected
+timeout /t 5 >nul
+echo [ ] Finding snapchat
+timeout /t 1 >nul
+echo [!] Snapchat Found
+echo [ ] Attempting first crack
+timeout /t 5 >nul
+echo [!] Cracking failed! (this is normal)
+echo [ ] Attempting second crack
+timeout /t 2 >nul
+echo [!] Cracked!
+timeout /t 2 >nul
+echo [!] Password Found
+timeout /t 1 >nul
+echo [!] Cracking finished
+pause
+goto password
+
+:password
+cls
+echo Username: this is meant for shits and lols 
+echo Password: this is meant for shits and lols
+pause
